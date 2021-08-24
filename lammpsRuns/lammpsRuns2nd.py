2f6c45a9289ab2d4a4d571878153eda58f0ffca1
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-08-24 11:41:02 +0200

    update

--- lammpsRuns/lammpsRuns2nd.py
+++ lammpsRuns/lammpsRuns2nd.py
@@ -31,10 +31,10 @@ if __name__ == '__main__':
         import numpy as np
 
 	nruns	 = 1
-	nThreads = 9
+	nThreads = 1 #9
 	nNode	 = 1
-	jobname  = 'HeaNiCoCrTakeOneOutRlxd2nd'
-        sourcePath = os.getcwd() + '/../postprocess/HeaNiCoCrTakeOneOut' #--- must be different than sourcePath
+	jobname  = 'testTakeOneOutRlxd'
+        sourcePath = os.getcwd() + '/../postprocess/testTakeOneOut' #--- must be different than sourcePath
 	EXEC_DIR = '/home/kamran.karimi1/Project/git/lammps2nd/lammps/src' #--- path for executable file
 	MEAM_library_DIR='/home/kamran.karimi1/Project/git/CrystalPlasticity/testRuns/dataFiles' #--- meam potential parameters
         SCRPT_DIR = os.getcwd()+'/lmpScripts'
