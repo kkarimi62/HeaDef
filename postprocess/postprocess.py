eacfe75f1f9aed2170ff0b29d7a1c5b9a04ff103
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-08-31 15:42:05 +0200

    update

--- postprocess/postprocess.py
+++ postprocess/postprocess.py
@@ -15,9 +15,9 @@ if __name__ == '__main__':
 	import os
 #
 	nruns	 = range(1)
-	jobname  = ['HeaNiCoCrNatom10KTakeOneOutFreeze','HeaNiCoCrTakeOneOut', 'NiNatom1KTakeOneOut'][2]
+	jobname  = ['HeaNiCoCrNatom10KTakeOneOutFreeze','HeaNiCoCrTakeOneOut', 'NiNatom100KTakeOneOut'][2]
 	DeleteExistingFolder = True
-	readPath = os.getcwd() + '/../lammpsRuns/NiNatom1K'#HeaNiCoCr' #--- source
+	readPath = os.getcwd() + '/../lammpsRuns/NiNatom100K'#HeaNiCoCr' #--- source
 	EXEC_DIR = '.'     #--- path for executable file
 	durtn = '00:14:59'
 	mem = '8gb'
