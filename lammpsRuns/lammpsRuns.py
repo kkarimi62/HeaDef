de27a834d04ca93670559cbcfe31fb42d3977ed4
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-08-31 09:36:40 +0200

    update

--- lammpsRuns/lammpsRuns.py
+++ lammpsRuns/lammpsRuns.py
@@ -1,26 +1,17 @@
-def makeOAR( EXEC_DIR, node, core, time, PYFIL ):
+def makeOAR( EXEC_DIR, node, core, time):
 	someFile = open( 'oarScript.sh', 'w' )
 	print >> someFile, '#!/bin/bash\n'
 	print >> someFile, 'EXEC_DIR=%s\n' %( EXEC_DIR )
 	print >> someFile, 'MEAM_library_DIR=%s\n' %( MEAM_library_DIR )
 	print >> someFile, 'module load mpich/3.2.1-gnu\n'
 
-        '''
-	#--- run python script
-	pyScript = open( '%s/pyScript.py'%writPath, 'w' )
-	print >> pyScript, 'import imp\ngn=imp.load_source(\'generate.name\',\'%s/generate.py\')'%(PYFIL)
-	print >> pyScript, 'gn.Generate( %s, %s, %s,title = \'data.txt\',ratio1 = %s, ratio2 = %s, ratio3 = %s, ratio4 = %s, ratio5 = %s )'%(natom, ntypes, rho, 0.05, 0.26, 0.02, 0.4, 0.27)
-	pyScript.close()
-	print >> someFile, 'python pyScript.py\n'
-        '''
-
 	#--- run python script 
 	OUT_PATH = '.'
 	if SCRATCH:
 		OUT_PATH = '/scratch/${SLURM_JOB_ID}'
 	if EXEC == 'lmp_mpi':
-            for script in [ 'file.in', 'filee.in','mini.lmp']: #,'Thermalization.lmp', 'vsgc.lmp']:
-                print >> someFile, "mpirun -np %s $EXEC_DIR/%s < %s -echo screen -var OUT_PATH %s -var cutoff %s -var natoms %s -var PathEam %s"%(nThreads*nNode, EXEC, script, OUT_PATH,cutoff, natom, '%s/%s'%(MEAM_library_DIR,'Ni_u3.eam'))
+            
+		print >> someFile, "mpirun -np %s $EXEC_DIR/%s < %s -echo screen -var OUT_PATH %s -var cutoff %s -var natoms %s -var PathEam %s"%(nThreads*nNode, EXEC, lmpScript, OUT_PATH,cutoff, natom, MEAM_library_DIR )
 	someFile.close()										  
 
 
@@ -29,14 +20,18 @@ if __name__ == '__main__':
         import numpy as np
 
 	nruns	 = 1
+	#
 	nThreads = 9
 	nNode	 = 1
+	#
 	jobname  = ['HeaNiCoCrNatom100K','NiNatom10K'][1]
+	#
 	EXEC_DIR = '/home/kamran.karimi1/Project/git/lammps2nd/lammps/src' #--- path for executable file
-#	MEAM_library_DIR='/home/kamran.karimi1/Project/git/CrystalPlasticity/testRuns/dataFiles' #--- meam potential parameters
+	#
 	MEAM_library_DIR='/home/kamran.karimi1/Project/git/lammps2nd/lammps/potentials'
         SCRPT_DIR = os.getcwd()+'/lmpScripts'
-	PYFIL = '/home/kamran.karimi1/Project/git/CrystalPlasticity/py'
+	LmpScript = ['Ni/PrepTemp0.in'][0]
+	#
 	EXEC = 'lmp_mpi' #'lmp_serial'
 	durtn = '23:59:59' #'167:59:59'
 	SCRATCH = None
@@ -49,7 +44,7 @@ if __name__ == '__main__':
         cutoff = 3.52 #3.58
     #   cutoffs = np.linspace((1.0-0.5)*cutoff,(1+0.5)*cutoff,nruns)
 #	rho = 0.1
-	#--- update filee.in 
+
 	#---
 	os.system( 'rm -rf %s' % jobname ) #--- rm existing
 	os.system( 'rm jobID.txt' )
@@ -64,9 +59,10 @@ if __name__ == '__main__':
 			path=os.getcwd() + '/%s' % ( jobname)
 			os.system( 'cp %s/%s %s' % ( EXEC_DIR, EXEC, path ) ) # --- create folder & mv oar scrip & cp executable
 		#---
-		os.system( 'cp %s/file.in %s/filee.in %s/mini.lmp %s/Thermalization.lmp %s/vsgc.lmp %s/NiCoCr.lammps.eam  %s' %(SCRPT_DIR,SCRPT_DIR,SCRPT_DIR,SCRPT_DIR,SCRPT_DIR,MEAM_library_DIR, writPath) ) #--- lammps script: periodic x, pxx, vy, load
+		os.system( 'cp %s/%s %s/lmpScript.txt' %( SCRPT_DIR, LmpScript, writPath) ) #--- lammps script: periodic x, pxx, vy, load
+
 		#---
-		makeOAR( path, 1, nThreads, durtn, PYFIL ) # --- make oar script
+		makeOAR( path, 1, nThreads, durtn) # --- make oar script
 		os.system( 'chmod +x oarScript.sh; mv oarScript.sh %s' % ( writPath) ) # --- create folder & mv oar scrip & cp executable
 		os.system( 'sbatch --partition=%s --mem=%s --time=%s --job-name %s.%s --output %s.%s.out --error %s.%s.err \
 						    --chdir %s -c %s -n %s %s/oarScript.sh >> jobID.txt'\
