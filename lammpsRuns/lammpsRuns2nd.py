1a8484d222366d127ac97852faf059b541d9a378
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-10-20 00:57:57 +0200

    update

--- lammpsRuns/lammpsRuns2nd.py
+++ lammpsRuns/lammpsRuns2nd.py
@@ -12,7 +12,8 @@ def makeOAR( EXEC_DIR, node, core, time ):
 #	 print >> someFile, "$EXEC_DIR/%s < in.txt -var OUT_PATH %s -var MEAM_library_DIR %s"%( EXEC, OUT_PATH, MEAM_library_DIR )
 #	cutoff = 1.0 / rho ** (1.0/3.0)
 	if EXEC == 'lmp_mpi' or EXEC == 'lmp_serial':
-		print >> someFile, "mpirun -np %s $EXEC_DIR/%s < %s -echo screen -var OUT_PATH %s -var PathEam %s %s"%(nThreads*nNode, EXEC, 'lmpScript.txt', OUT_PATH, MEAM_library_DIR, Variables)
+		for script,var,indx in zip(Pipeline,Variables,range(100)):
+		print >> someFile, "mpirun -np %s $EXEC_DIR/%s < %s -echo screen -var OUT_PATH %s -var PathEam %s %s"%(nThreads*nNode, EXEC, 'lmpScript%s.txt'%indx, OUT_PATH, MEAM_library_DIR, var)
 	someFile.close()										  
 
 
@@ -27,9 +28,9 @@ if __name__ == '__main__':
 	#
 	jobname  = {1:'NiNatom100KReplaceCoRlxd',2:'NiCoCrNatom100KTemp300', 3:'NiNatom1KT0EdgeDisl', 4:'NiCoCrNatom1000KEdgeDisl', 5:'NiCoCrNatom200KTemp600Annealed', 6:'NiCoCrNatom100KTemp300Gdot4',7:'NiNatom1KT300EdgeDisl'}[7]
 	sourcePath = os.getcwd() +\
-				{1:'/../postprocess/NiCoCrNatom1K',2:'/NiCoCrNatom100K',3:'/NiCoCrNatom100KTemp300',4:'/junk',5:'/../postprocess/NiNatom1KEdgeDisl',6:'/../postprocess/NiCoCrNatom1K', 7:'/../postprocess/NiCoCrNatom1000K', 8:'/NiCoCrNatom200KTemp600', 9:'/NiNatom1KT0EdgeDisl'}[9] #--- must be different than sourcePath
+				{1:'/../postprocess/NiCoCrNatom1K',2:'/NiCoCrNatom100K',3:'/NiCoCrNatom100KTemp300',4:'/junk',5:'/../postprocess/NiNatom1KEdgeDisl',6:'/../postprocess/NiCoCrNatom1K', 7:'/../postprocess/NiCoCrNatom1000K', 8:'/NiCoCrNatom200KTemp600', 9:'/NiNatom1KT0EdgeDisl'}[5] #--- must be different than sourcePath
         #
-        sourceFiles = {1:['Equilibrated_300.dat'],2:['data.txt','ScriptGroup.txt'],3:['data.txt'], 4:['data_minimized.txt']}[4] #--- to be copied from the above directory
+        sourceFiles = {1:['Equilibrated_300.dat'],2:['data.txt','ScriptGroup.txt'],3:['data.txt'], 4:['data_minimized.txt']}[3] #--- to be copied from the above directory
 	#
 	EXEC_DIR = '/home/kamran.karimi1/Project/git/lammps2nd/lammps/src' #--- path for executable file
 	#
@@ -39,7 +40,12 @@ if __name__ == '__main__':
 	Alloy = {1:'Ni', 2:'NiCoCr'}[1]
 	#
 	LmpScript = '%s/'%Alloy+{1:'relax.in', 2:'relaxWalls.in', 3:'Thermalization.lmp', 4:'vsgc.lmp', 5:'minimization_edge.lmp', 6:'shearDispTemp.in', 7:'Thermalization_edge.lmp'}[7] #--- [pbc, rigid walls,] 
-	Variables = {6:' -var T 300 -var DataFile Equilibrated_300.dat',5:' -var buff 6.0 -var nevery 1', 7:' -var buff 6.0 -var T 300 -var DataFile data_minimized.txt'}[7] 
+	#
+	Variable = {6:' -var T 300 -var DataFile Equilibrated_300.dat',5:' -var buff 6.0 -var nevery 1', 7:' -var buff 6.0 -var T 300 -var DataFile data_minimized.txt'}[7] 
+	#--- different scripts in a pipeline
+	indices = [5,7]
+	Pipeline = list(map(lambda x:'%s/'%Alloy+LmpScript[x],indices))
+	Variables = list(map(lambda x:Variable[x], indices]))
 	#
 	EXEC = ['lmp_mpi','lmp_serial'][0]
 	durtn = '23:59:59'
@@ -61,7 +67,8 @@ if __name__ == '__main__':
 			path=os.getcwd() + '/%s' % ( jobname)
 			os.system( 'cp %s/%s %s' % ( EXEC_DIR, EXEC, path ) ) # --- create folder & mv oar scrip & cp executable
 		#---
-		os.system( 'cp %s/%s %s/lmpScript.txt' %( SCRPT_DIR, LmpScript, writPath) ) #--- lammps script: periodic x, pxx, vy, load
+		for script,indx in zip(Pipeline,range(100)):
+			os.system( 'cp %s/%s %s/lmpScript%s.txt' %( SCRPT_DIR, script, writPath, indx) ) #--- lammps script: periodic x, pxx, vy, load
                 for sf in sourceFiles:
         		os.system( 'cp %s/Run%s/%s %s' %(sourcePath, irun, sf, writPath) ) #--- lammps script: periodic x, pxx, vy, load
 		#---
