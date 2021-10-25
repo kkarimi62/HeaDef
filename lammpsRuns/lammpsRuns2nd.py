d63798ac72589f90bdce8280a43275a1fe70e877
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-10-25 15:24:21 +0200

    update

--- lammpsRuns/lammpsRuns2nd.py
+++ lammpsRuns/lammpsRuns2nd.py
@@ -23,7 +23,7 @@ if __name__ == '__main__':
 
 	nruns	 = 1
 	#
-	nThreads = [1,9][0]
+	nThreads = [1,9][1]
 	nNode	 = 1
 	#
 	jobname  = {
@@ -34,7 +34,7 @@ if __name__ == '__main__':
 				5:'NiCoCrNatom200KTemp600Annealed', 
 				6:'NiCoCrNatom100KTemp300Gdot4',
 				7:'NiNatom1KT300EdgeDisl',
-				8:'NiCoCrNatom10KT0Elastic2nd',
+				8:'NiCoCrNatom1KT0Elastic',
 			   }[8]
 	sourcePath = os.getcwd() +\
 				{	
@@ -48,7 +48,7 @@ if __name__ == '__main__':
 					8:'/NiCoCrNatom200KTemp600', 
 					9:'/NiNatom1KT0EdgeDisl',
 					10:'/NiCoCrNatom10KT0Elastic',
-				}[10] #--- must be different than sourcePath
+				}[4] #--- must be different than sourcePath
         #
 	sourceFiles = { 0:False,
 					1:['Equilibrated_300.dat'],
@@ -56,7 +56,7 @@ if __name__ == '__main__':
 					3:['data.txt'], 
 					4:['data_minimized.txt'],
 					5:['data_init.txt','ScriptGroup.0.txt'], #--- only one partition! for multiple ones, use 'submit.py'
-				 }[5] #--- to be copied from the above directory
+				 }[0] #--- to be copied from the above directory
 	#
 	EXEC_DIR = '/home/kamran.karimi1/Project/git/lammps2nd/lammps/src' #--- path for executable file
 	#
@@ -84,20 +84,20 @@ if __name__ == '__main__':
 				} 
 	#
 	Variable = {
-				0:' -var natoms 10000 -var cutoff 3.52  -var DumpFile dumpInit.xyz -var WriteData data_init.txt',
+				0:' -var natoms 1000 -var cutoff 3.52  -var DumpFile dumpInit.xyz -var WriteData data_init.txt',
 				6:' -var T 300 -var DataFile Equilibrated_300.dat',
 				5:' -var DataFile data.txt -var buff 6.0 -var DumpFile dumpMin.xyz -var nevery 1 -var WriteData data_minimized.txt', 
 				7:' -var buff 6.0 -var T 0.1 -var DataFile data_minimized.txt -var DumpFile dumpThermalized.xyz -var WriteData Equilibrated_300.dat',
 				8:' -var buff 6.0 -var T 0.1 -var DataFile Equilibrated_300.dat -var DumpFile dumpSheared.xyz',
 				9:' -var natoms 1000 -var cutoff 3.52 -var INC %s'%(SCRPT_DIR),
-				10:' -var DataFile data_init.txt -var AtomGroup ScriptGroup.0.txt -var INC %s'%(SCRPT_DIR),
-				'p0':' data_init.txt 4.0 %s'%(os.getcwd()+'/../postprocess'),
+				10:' -var DataFile data_init.txt -var INC %s'%(SCRPT_DIR),
+				'p0':' data_init.txt 12.0 %s'%(os.getcwd()+'/../postprocess'),
 				} 
 	#--- different scripts in a pipeline
 	indices = {
 				0:[5,7,8], #--- put disc. by atomsk, minimize, thermalize, and shear
 				1:[9],     #--- elastic constants
-				2:[[0,'p0'],[10]][1],	   #--- local elastic constants: [0]-> prepare sample & partition [1]->deform
+				2:[[0,'p0',10]],	   #--- local elastic constants: [0]-> prepare sample & partition [1]->deform
 			  }[2]
 	Pipeline = list(map(lambda x:LmpScript[x],indices))
 	Variables = list(map(lambda x:Variable[x], indices))
@@ -106,7 +106,7 @@ if __name__ == '__main__':
 	EXEC_lmp = ['lmp_mpi','lmp_serial'][0]
 	durtn = '23:59:59'
 	mem = '8gb'
-	partition = ['gpu-v100','parallel','cpu2019','single'][-1]
+	partition = ['gpu-v100','parallel','cpu2019','single'][1]
 	#---
 	os.system( 'rm -rf %s' % jobname ) #--- rm existing
 	os.system( 'rm jobID.txt' )
