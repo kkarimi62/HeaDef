34d73add1a9679fe876bf9c9a89feca426e4fc85
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-10-26 08:49:24 +0200

    update

--- lammpsRuns/lammpsRuns2nd.py
+++ lammpsRuns/lammpsRuns2nd.py
@@ -104,7 +104,7 @@ if __name__ == '__main__':
 	EXEC = list(map(lambda x:'lmp' if type(x) == type(0) else 'py', indices))	
 	#
 	EXEC_lmp = ['lmp_mpi','lmp_serial'][0]
-	durtn = '23:59:59'
+	durtn = ['23:59:59','00:59:59'][1]
 	mem = '8gb'
 	partition = ['gpu-v100','parallel','cpu2019','single'][1]
 	#---
