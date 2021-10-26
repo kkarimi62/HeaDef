20bbc4e179719cc2aabead2a6a3e855ebfc34197
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-10-27 00:07:47 +0200

    update

--- lammpsRuns/lammpsRuns2nd.py
+++ lammpsRuns/lammpsRuns2nd.py
@@ -85,7 +85,7 @@ if __name__ == '__main__':
 				} 
 	#
 	Variable = {
-				0:' -var natoms 1000 -var cutoff 3.52  -var DumpFile dumpInit.xyz -var WriteData data_init.txt',
+				0:' -var natoms 10000 -var cutoff 3.52  -var DumpFile dumpInit.xyz -var WriteData data_init.txt',
 				6:' -var T 300 -var DataFile Equilibrated_300.dat',
 				5:' -var DataFile data.txt -var buff 6.0 -var DumpFile dumpMin.xyz -var nevery 1 -var WriteData data_minimized.txt', 
 				7:' -var buff 6.0 -var T 0.1 -var DataFile data_minimized.txt -var DumpFile dumpThermalized.xyz -var WriteData Equilibrated_300.dat',
