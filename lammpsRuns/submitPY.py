6faba3ae1ef765be06e19f908b3bfed84f7e7c85
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2021-10-27 08:54:50 +0200

    update

--- lammpsRuns/submitPY.py
+++ lammpsRuns/submitPY.py
@@ -3,7 +3,7 @@ if __name__ == '__main__':
 	import os
 	import numpy as np
 	#---
-	lnums = [ 37, 58, 94 ]
+	lnums = [ 37, 95 ]
 	string=open('lammpsRuns2nd.py').readlines() #--- python script
 	#---
 	PHI = [23.1142383485014, 11.5571191742507, 7.704746116167133, 5.136497410778088, 3.302034049785914] #range(1331) #np.logspace(np.log10(0.1),np.log10(0.9),8)
@@ -15,11 +15,11 @@ if __name__ == '__main__':
 #		string[ inums ] = "\tjobname=\'NiCoCrNatom1KT0Elastic2nd%s\'\n" % (iphi) #--- change job name
 		string[ inums ] = "\t8:\'NiCoCrNatom10KT0Elastic%s\',\n" % (iphi) #--- change job name
 		#---	densities
-		inums = lnums[ 1 ] - 1
-		string[ inums ] = "\t5:[\'data_init.txt\',\'ScriptGroup.%s.txt\'],\n" % (iphi) #--- change job name
+#		inums = lnums[ 1 ] - 1
+#		string[ inums ] = "\t5:[\'data_init.txt\',\'ScriptGroup.%s.txt\'],\n" % (iphi) #--- change job name
 #		string[ inums ] = "\tsourcePath = os.getcwd() +\'/../postprocess/HeaNiCoCrNatom10KTakeOneOutFreezeFract%s\'\n" % (iphi) #--- change job name
 		#---	densities
-		inums = lnums[ 2 ] - 1
+		inums = lnums[ 1 ] - 1
 		string[ inums ] = "\t\'p0\':\' data_init.txt %s"%PHI[iphi]+" %s\'%(os.getcwd()+\'/../postprocess\'),\n" #--- change job name
 		
 		sfile=open('junk%s.py'%iphi,'w');sfile.writelines(string);sfile.close()
