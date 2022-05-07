f456cc6083ed892180c65259667d44f9944d116b
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-05-07 16:37:44 +0200

    update

--- postprocess/postprocessNcbj.py
+++ postprocess/postprocessNcbj.py
@@ -18,18 +18,18 @@ if __name__ == '__main__':
 	jobname  = {
 				0:'NiCoCrNatom100KTakeOneOutRlxd',
 				1:'NiCoCrNatom200KTemp600Annealed', 
-				2:'NiCoCrNatom100KTemp800',
+				2:'NiCoCrNatom100KTemp1400',
 				}[2]
 	DeleteExistingFolder = True
 	readPath = os.getcwd() + {
 								0:'/../testRuns/glassCo5Cr2Fe40Mn27Ni26',
-								1:'/../lammpsRuns/AmirData/shengAnnealed/Temp800',
+								1:'/../lammpsRuns/AmirData/shengAnnealed/Temp1400',
 							}[1] # --- source
 	EXEC_DIR = '.'     #--- path for executable file
 	durtn = '04:59:59'
 	resources = {'mem':'64gb', 'partition':['o12h','a12h','i12h'][2],'nodes':1,'ppn':8}
 	argv = "path=%s"%(readPath) #--- don't change! 
-	argv2nd = "indx=7\ntemperature=800" 
+	argv2nd = "indx=7\ntemperature=1400" 
 	PYFILdic = { 
 		0:'pressFluc2nd.ipynb',
 		}
