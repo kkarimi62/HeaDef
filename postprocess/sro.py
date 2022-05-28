445f8f65245b5d3de50f030226a00472db8a9725
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-05-28 22:43:35 +0200

    update

---
+++ postprocess/sro.py
@@ -0,0 +1,37 @@
+if __name__ == '__main__':
+	import sys
+	import os
+	import numpy as np
+	#---
+	lnums = [ 22, 27, 33 ]
+	string=open('postprocessNcbj.py').readlines() #--- python script
+	#---
+	PHI={
+	0:400, 
+	1:600, 
+	2:800, 
+	3:1000, 
+	4:1200, 
+	5:1400,
+	}
+	nphi = len(PHI)
+	#---
+	count = 0
+	for key in PHI:
+		val = PHI[key]
+		#---	
+		inums = lnums[ 0 ] - 1
+#		string[ inums ] = "\t2:\'NiCoCrNatom100KTemp%s\',\n" % (PHI[iphi]) #--- change job name
+		string[ inums ] = "\t3:\'NiCoCrNatom100KTemp%ssro\',\n" % (val) #--- change job name
+		#---	
+		inums = lnums[ 1 ] - 1
+		string[ inums ] = "\t1:\'/../lammpsRuns/AmirData/shengAnnealed/Temp%s\',\n" % (val) #--- change job name
+		#---	densities
+		inums = lnums[ 2 ] - 1
+#		string[ inums ] = "\targv2nd = \'indx=7\\ntemperature=%s\'\n"%(PHI[iphi])
+		string[ inums ] = "\targv2nd = \'indx=%s\\ntemperature=%s\\nload=%s\'\n"%(7,val,500)
+
+		sfile=open('junk%s.py'%count,'w');sfile.writelines(string);sfile.close()
+		os.system( 'python3 junk%s.py'%count )
+		os.system( 'rm junk%s.py'%count )
+		count += 1
