a23b4d696b2793dd929b6669ec16b7ba09574d8d
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-06-04 17:51:51 +0200

    update

--- postprocess/postprocess.py
+++ postprocess/postprocess.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
 					8:'NiNatom1KEdgeDisl',
 					9:'NiCoCrNatom1KT0Elastic',
 					10:'FeNiT300Elasticity',
-					11:'NiCoCrNatom100KTemp600',
+					11:'NiCoCrNatom100KTemp600sro',
 				}[11]
 	DeleteExistingFolder = False,
 	readPath = os.getcwd() + {
