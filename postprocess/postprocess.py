2cba39a4cc6159337ea868ee7e3b9f5d14dd244b
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-06-06 06:51:43 +0200

    update

--- postprocess/postprocess.py
+++ postprocess/postprocess.py
@@ -28,7 +28,7 @@ if __name__ == '__main__':
 					10:'FeNiT300Elasticity',
 					11:'NiCoCrNatom100KTemp400sro',
 				}[11]
-	DeleteExistingFolder = False
+	DeleteExistingFolder = True
 	readPath = os.getcwd() + {
 								1:'/../lammpsRuns/AmirData/shengAnnealed/Temp400', #--- source
 							}[1]
