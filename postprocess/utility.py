0bcc00388ee5c195e96620d772a3060cb5289d93
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2022-08-31 06:19:42 +0200

    update

--- postprocess/utility.py
+++ postprocess/utility.py
@@ -712,7 +712,7 @@ def PrintOvito( cordc, sfile, footer, attr_list=['x','y','z'] ):
 def PltBitmap( value,
               xlabel = 'x', ylabel = 'y',
               xlim = (-0.5,0.5), ylim = (-0.5,0.5),
-              frac = 1.0, #--- plot a patch
+#              frac = 1.0, #--- plot a patch
               zscore = True,
               title = 'cxy.png',
               colorbar=False,
@@ -736,24 +736,37 @@ def PltBitmap( value,
     #
     fig = plt.figure(figsize=(4,4))
     ax = fig.add_subplot(111)
-    ax.set_xlabel(xlabel,fontsize=20)
-    ax.set_ylabel(ylabel,fontsize=20)
-    ax.tick_params(labelsize=20,which='both',axis='both', top=True, right=True)
+    fontsize=20 if not 'fontsize' in kwargs else kwargs['fontsize']
+    ax.set_xlabel(xlabel,fontsize=fontsize)
+    ax.set_ylabel(ylabel,fontsize=fontsize)
+    ax.tick_params(labelsize=fontsize,which='both',axis='both', top=True, right=True)
 #     ax.axes.xaxis.set_visible(False)
 #     ax.axes.yaxis.set_visible(False)
     #
     origin = kwargs['origin'] if 'origin' in kwargs else 'lower'
     pos = ax.imshow(val.real,cmap='bwr' if not 'cmap' in kwargs else kwargs['cmap'],
-                     extent=(xlim[0],xlim[1],ylim[0],ylim[1]),origin=origin ,vmin=vmin, vmax=vmax)
-    ax.set_xlim(xlim[0]*frac,xlim[1]*frac)
-    ax.set_ylim(ylim[0]*frac,ylim[1]*frac)
+                     extent=(xlim[0],xlim[1],ylim[0],ylim[1]),origin=origin ,
+                    vmin=vmin, vmax=vmax,
+                   interpolation=None if not 'interpolation' in kwargs else kwargs['interpolation']
+                   )
+    if 'frac' in kwargs:
+        frac = kwargs['frac']
+        ax.set_xlim(xlim[0]*frac,xlim[1]*frac)
+        ax.set_ylim(ylim[0]*frac,ylim[1]*frac)
+    if 'fracx' in kwargs:
+        fracx = kwargs['fracx']
+        ax.set_xlim(fracx)
+    if 'fracy' in kwargs:
+        fracy = kwargs['fracy']
+        ax.set_ylim(fracy)
 
     if colorbar:
-        fig.colorbar( pos, fraction = 0.04)
+        fig.colorbar( pos, pad=0.05 if not 'pad' in kwargs else kwargs['pad'], 
+					shrink=0.5,fraction = 0.04, orientation='vertical' if not 'orientation' in kwargs else kwargs['orientation'] )
     if 'DrawFrame' in kwargs: 
         DrawFrame(ax, *kwargs['DrawFrame'])
     if 'set_title' in kwargs:
-        ax.set_title(kwargs['set_title'],fontsize=20)
+        ax.set_title(kwargs['set_title'],fontsize=fontsize)
     #
     LOGY = True if ('yscale' in kwargs and kwargs['yscale'] == 'log') else False
     LOGX = True if ('xscale' in kwargs and kwargs['xscale'] == 'log') else False
