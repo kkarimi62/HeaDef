2ad3a443f797854971171e8e7cc838e8af437377
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-04-22 12:20:29 +0200

    updates

--- postprocess/postprocessNcbj.py
+++ postprocess/postprocessNcbj.py
@@ -13,52 +13,65 @@ def makeOAR( EXEC_DIR, node, core, partitionime, PYFIL, argv):
 	#---
 	someFile = open( 'oarScript.sh', 'w' )
 	print('#!/bin/bash\n', file=someFile)
-	print('EXEC_DIR=%s\n' %( EXEC_DIR ), file=someFile)
 	print('module load python-jupyter/dev-x86_64-gcc46-python35\n',file=someFile)
-	print('jupyter nbconvert --execute $EXEC_DIR/%s --to html --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;ls output.html'%(PYFIL), file=someFile)
+	print('EXEC_DIR=%s\n source /mnt/opt/spack-0.17/share/spack/setup-env.sh\n\nspack load python@3.8.12%%gcc@8.3.0\n\n'%( EXEC_DIR ),file=someFile)
+	if convert_to_py:
+		print('ipython3 py_script.py\n',file=someFile)
+		 
+	else:
+		print('jupyter nbconvert --execute $EXEC_DIR/%s --to html --ExecutePreprocessor.timeout=-1 --ExecutePreprocessor.allow_errors=True;ls output.html'%(PYFIL), file=someFile)
 	someFile.close()										  
+#
 #
 if __name__ == '__main__':
 	import os
-
-	nruns	 = range(1)
+#
+	runs	 = range(1)
+	nNode    = 1
+	nThreads = 1
 	jobname  = {
-				0:'NiCoCrNatom100KDistortions/Temp600',
-				1:'NiCoCrNatom100KTemp600AgingSro', 
-				2:'NiCoCrNatom100KTemp600',
-				3:'NiCoCrNatom100KTemp600/dislocated/load600',
-				4:'NiCoCrNatom100KTemp600Rss/dislocated/load600',
-				}[1]
+				'1':'nicocrnicocrNatom100KMultipleTempIrradiatedAnneal/benchmark/temp0',
+				}['1']
 	DeleteExistingFolder = False
 	readPath = os.getcwd() + {
-								0:'/../testRuns/glassCo5Cr2Fe40Mn27Ni26',
-								1:'/../lammpsRuns/AmirData/shengAnnealed/Temp600/dislocated',
-								2:'/../lammpsRuns/AmirData/shengRss/Temp600/dislocated',
-								3:'/../lammpsRuns/AmirData/shengAnnealed/Temp600/dislocated',
-							}[3] # --- source
+								'1':'/../simulations/nicocrnicocrNatom100KMultipleTempIrradiatedAnneal/benchmark/temp0',
+ 							}['1'] #--- source
 	EXEC_DIR = '.'     #--- path for executable file
-	durtn = '11:59:59'
-	resources = {'mem':'128gb', 'partition':['o12h','a12h','i12h'][2],'nodes':1,'ppn':1}
+#	py_library_directory = '$HOME/Project/git/HeaDef/postprocess' 
+	durtn = '23:59:59'
+	mem = '32gb'
+	partition = ['INTEL_PHI','INTEL_HASWELL'][0] 
+	argv = "%s"%(readPath) #--- don't change! 
 	PYFILdic = { 
-		0:'pressFluc2nd.ipynb',
+		0:'avalancheAnalysis.ipynb',
 		}
 	keyno = 0
+	convert_to_py = True
+	outputPath = '.'
 #---
 #---
-	PYFIL = PYFILdic[ keyno ] 
+	PYFIL = PYFILdic[ keyno ]
+	if convert_to_py:
+		os.system('jupyter nbconvert --to script %s --output py_script\n'%PYFIL)
+		PYFIL = 'py_script.py'
 	#--- update argV
 	#---
 	if DeleteExistingFolder:
+		print('rm %s'%jobname)
 		os.system( 'rm -rf %s' % jobname ) # --- rm existing
 	# --- loop for submitting multiple jobs
-	for counter in nruns:
+	os.system( 'rm jobID.txt' )
+	for counter in runs:
 		print(' i = %s' % counter)
 		writPath = os.getcwd() + '/%s/Run%s' % ( jobname, counter ) # --- curr. dir
 		os.system( 'mkdir -p %s' % ( writPath ) ) # --- create folder
-		os.system( 'cp configuration.ini LammpsPostProcess*.py OvitosCna.py utility*.py %s' % ( writPath ) ) #--- cp python module
-		makeOAR( writPath, 1, 1, durtn, PYFIL, readPath+"/Run%s"%counter) # --- make oar script
-		os.system( 'chmod +x oarScript.sh; cp oarScript.sh configuration.ini %s; cp %s/%s %s' % ( writPath, EXEC_DIR, PYFIL, writPath ) ) # --- create folder & mv oar scrip & cp executable
-		os.system( 'qsub -q %s -l nodes=%s:ppn=%s -l walltime=%s -N %s.%s -o %s -e %s -d %s  %s/oarScript.sh'\
-			%( resources['partition'], resources['nodes'], resources['ppn'], durtn, jobname, counter, writPath, writPath, writPath , writPath ) ) # --- runs oarScript.sh!
-											 
+#		os.system( 'cp utility.py LammpsPostProcess2nd.py OvitosCna.py %s' % ( writPath ) ) #--- cp python module
+		makeOAR( writPath, 1, 1, durtn, PYFIL, argv+"/Run%s"%counter) # --- make oar script
+		os.system( 'chmod +x oarScript.sh; mv oarScript.sh %s; cp configuration.ini %s;cp %s/%s %s' % ( writPath, writPath, EXEC_DIR, PYFIL, writPath ) ) # --- create folder & mv oar scrip & cp executable
+		os.system( 'sbatch --partition=%s --mem=%s --time=%s --job-name %s.%s --output %s.%s.out --error %s.%s.err \
+                                 --chdir %s --ntasks-per-node=%s --nodes=%s %s/oarScript.sh >> jobID.txt'\
+                            % ( partition, mem, durtn, jobname.split('/')[0], counter, jobname.split('/')[0], counter, jobname.split('/')[0], counter \
+                                , writPath, nThreads, nNode, writPath ) ) # --- runs oarScript.sh! 
+	os.system( 'mv jobID.txt %s' % ( os.getcwd() + '/%s' % ( jobname ) ) )
+
 
