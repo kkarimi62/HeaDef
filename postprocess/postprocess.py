c33df9ffaa40777be5184a9240c0dbc4af0ef37f
kkarimi62 <51524428+kkarimi62@users.noreply.github.com>
2023-05-28 20:07:33 +0200

    update

--- postprocess/postprocess.py
+++ postprocess/postprocess.py
@@ -48,7 +48,7 @@ if __name__ == '__main__':
 					17:'RSS_before_indentation',
 					18:'RSS_compressed',
 					19:'nicocrNatom100KMultipleTempIrradiatedAnneal/benchmark/temp0',
-					20:'AmirData/NiAl/Swapped_300',
+					20:'AmirData/NiAl/annealing',
 					21:'AmirData/indentation',
 				}[20]
 	DeleteExistingFolder = True
@@ -63,7 +63,7 @@ if __name__ == '__main__':
 								17:'/../lammpsRuns/AmirData/RSS_before_indentation',
 								18:'/../lammpsRuns/AmirData/RSS_compressed',
 								19:'/../lammpsRuns/nicocrNatom100KMultipleTempIrradiatedAnneal/benchmark/temp0',
-								20:'/../lammpsRuns/AmirData/NiAl/Swapped_300',
+								20:'/../lammpsRuns/AmirData/NiAl/annealing',
 								21:'/../lammpsRuns/AmirData/indentation',
 							}[20]
 	EXEC_DIR = '.'     #--- path for executable file
